6e4b0a7fa66c2550af52c076351759f9e5f7066c
truckboyshortfilm-collab <truckboy.short.film@gmail.com>
2026-05-07 00:16:27 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'truckboy-v1';
+const FILES = [
+  './拍攝總表.html',
+  './day1.html',
+  './day2.html',
+  './day3.html',
+  './day4.html',
+  './day5.html',
+  './製片工作清單.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@300;400;500;700;900&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => cached))
+  );
+});
