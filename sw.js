37e631e9e14a5591f4cbfbbcf75a7deba97df168
truckboyshortfilm-collab <truckboy.short.film@gmail.com>
2026-05-14 00:20:09 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'truckboy-v1';
+const CACHE = 'truckboy-v2';
 
 const STATIC = [
   './icon-192.png',
@@ -24,7 +24,11 @@ self.addEventListener('install', e => {
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(self.clients.claim());
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', e => {
